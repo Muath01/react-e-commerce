a2c38272a906da86a3940cafe1e653e0d13cbded
Muath <muath.khalifa@yahoo.com>
2023-01-01 09:00:45 +0000

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import { stringify } from 'querystring';
+import React, { ReactElement, ReactNode, useCallback, useState, FC } from 'react';
+import { Container } from 'react-bootstrap';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
-function App() {
+
+
+console.log("hello");
+
+
+const App:FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+
+    <h1>hello This is me</h1>
+        
+    </>
+  )
+
 }
 
 export default App;
